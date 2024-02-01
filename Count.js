9f309121f1ff7b7b73ffc4c06dfb84deb34cdea6
zaheenrakib <rakibislambest@gmail.com>
2024-02-01 10:47:53 +0600

    count a completed

--- Count.js
+++ Count.js
@@ -1,3 +1,12 @@
-const str = "I am a Webdeveloper";
+const str = "I am a Webdeveloper a";
+const count = "a";
 
-console.log(str["a"]);
+let counted = 0 ;
+
+for (i = 0;i < str.length ;i++){
+    if(str[i] === count){
+        counted++
+    }
+}
+
+console.log(`The Letter ${count} appers ${counted} Times`);
