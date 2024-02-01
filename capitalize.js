94e546e6bb4fa0add85583323c93425e26a71d75
zaheenrakib <rakibislambest@gmail.com>
2024-02-01 12:22:15 +0600

    Complete WithOut

--- capitalize.js
+++ capitalize.js
@@ -1,15 +1,48 @@
 const values = "i am a full stack web developer. i work with javascript and React";
 
-const capitalization = values.split(' ').map(sentence => {
-    const words = sentence.split(' ');
-    if(words.length > 0 ){
-        words[0] = words[0].charAt(0).toUpperCase() + words[0].slice(1);
-    }
-    return words.join( ' ' );
-}).join(' ');
 
 
-console.log(capitalization);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const values = "i am a full stack web developer. i work with javascript and React";
+
+// const capitalization = values.split(' ').map(sentence => {
+//     const words = sentence.split(' ');
+//     if(words.length > 0 ){
+//         words[0] = words[0].charAt(0).toUpperCase() + words[0].slice(1);
+//     }
+//     return words.join( ' ' );
+// }).join(' ');
+
+
+// console.log(capitalization);
 
 
 
---
+++ replace.js
@@ -0,0 +1,7 @@
+let values = "xXx";//yYy
+
+let replace = values.replaceAll("x","y");
+replace = replace.replaceAll("X","Y");
+
+
+console.log(replace);
